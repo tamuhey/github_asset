7d1c0333378084f4afcd5acd04aa3f1ffa46ee31
Yohei Tamura <tamuhey@gmail.com>
2020-02-03 15:24:21 +0900

    get without token for public repository

--- github_asset.py
+++ github_asset.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Dict, Optional
 import tqdm
 import os
 import re
@@ -43,8 +44,7 @@ def make_upload_header(token: str):
     return header
 
 
-def make_download_header(token: str):
-    header = auth_header(token)
+def make_download_header(header: Dict) -> Dict:
     header["Accept"] = "application/octet-stream"
     return header
 
@@ -72,7 +72,7 @@ def get_token() -> str:
 
 
 def up(file, tag, token: str = "", repo=None):
-    token = get_token()
+    token = token or get_token()
     repo = repo or get_repo()
     print(f"repo: {repo}")
     url = get_upload_url(tag, repo, token)
@@ -86,29 +86,36 @@ def up(file, tag, token: str = "", repo=None):
     print(res.json())
 
 
-def get(name: str, repo=None):
-    token = get_token()
-    header = auth_header(token)
+def check_maybe_private_repo(url: str) -> bool:
+    r = requests.get(url)
+    return r.status_code == 404
+
+
+def get(name: str, repo: Optional[str] = None, token: Optional[str] = None):
     repo = repo or get_repo()
     url = make_releases_url(repo)
+    if not token and check_maybe_private_repo(url):
+        token = get_token()
+    header = auth_header(token) if token else {}
     r = requests.get(url, headers=header)
     r.raise_for_status()
-    assets = []
-    for release in r.json():
-        assets.extend(release["assets"])
+
     target = None
-    for asset in assets:
-        if asset["name"] == name:
-            target = asset
+    for release in r.json():
+        for asset in release["assets"]:
+            if asset["name"] == name:
+                target = asset
+                break
+        if target is not None:
             break
+
     assert target, f"Asset {name} not found."
 
-    size = target["size"]
-    header = make_download_header(token)
+    header = make_download_header(header)
     r = requests.get(target["url"], headers=header, stream=True)
 
     chunk_size = 8192
-    with tqdm.tqdm(total=size) as p:
+    with tqdm.tqdm(total=target["size"]) as p:
         with open(name, "wb") as f:
             for c in r.iter_content(chunk_size=chunk_size):
                 if c:
--- test_github_asset.py
+++ test_github_asset.py
@@ -1,5 +1,15 @@
-from github_asset import get_repo
+from github_asset import get_repo, get
+import os
 
 
 def test_get_repo():
     assert get_repo() == "tamuhey/github_asset"
+
+
+def test_get_file():
+    fname = "foo.txt"
+    os.remove(fname)
+    get(fname)
+    assert os.path.exists(fname)
+    os.remove(fname)
+
